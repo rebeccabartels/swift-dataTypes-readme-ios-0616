7f42a371df930b17005abd5c02ef8cedee3ed249
rebeccabartels <rebeccakbartels@icloud.com>
2016-07-06 09:17:30 -0400

    interpolation plus planets equals funzo

--- Types.playground/Contents.swift
+++ Types.playground/Contents.swift
@@ -4,15 +4,22 @@ var ninthPlanet: String
 ninthPlanet = "Pluto"
 
 let fifthPlanet: String = "Jupiter"
-// fifthPlanet is a constant of type ______
+// fifthPlanet is a constant of type ______ STRING!
 
 let sixthPlanet: String = "Saturn"
-// sixthPlanet is a constant of type ______
+// sixthPlanet is a constant of type ______STRING!
 
 let seventhPlanet = "Uranus"
-// seventhPlanet is a constant of type ______
+// seventhPlanet is a constant of type ______ STRING!
 
 let numberOfCountriesInAfrica = 54
 
 let numberOfMoonsOfSaturn = 62
-print("There are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet)")
+print("There are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet)")   //INTERPOLATION 
+
+
+let firstPlanet = "Mercury"  //more playing in playground
+var secondPlanet: String
+secondPlanet = "Venus"
+
+
